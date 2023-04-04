2a0fbd9591d289e002d801a71819783bdef88fc7
Rithish <rithish.k@akshayahgs.org>
2023-04-04 13:43:59 +0530

    added navigation with svelte navigator

--- Hypothesis Testing/src/App.svelte
+++ Hypothesis Testing/src/App.svelte
@@ -1,47 +1,57 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+import {Link, Router} from "svelte-navigator";
+const tTestLinks: string[] = [
+  "Sample t test",
+  "Difference in means",
+  "Paired t test",
+  "Confidence interval"
+]
+const chiTestLinks: string[] = [
+  "Goodness of fit test",
+  "Contingency table"
+]
+const nonParametricTests: string[] = [
+  "Single sample sign",
+  "Wilcoxon signed rank",
+  "Paired sign",
+  "Matched pairs signed rank",
+  "Wilcoxon rank-sum"
+]
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<Router>
+  <nav>
+    <h2>t test</h2>
+    {#each tTestLinks as tTestLink}
+      <Link to={tTestLink}>{tTestLink}</Link>
+    {/each}
+    <h2>&chi;<sup>2</sup>-test</h2>
+    {#each chiTestLinks as chiTestLink}
+      <Link to={chiTestLink}>{chiTestLink}</Link>
+    {/each}
+    <h2>Non Parametric Tests</h2>
+    {#each nonParametricTests as nonParametricTest}
+      <Link to={nonParametricTest}>{nonParametricTest + " test"}</Link>
+    {/each}
+  </nav>
+  <main>
 
-  <div class="card">
-    <Counter />
-  </div>
+  </main>
+</Router>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+nav {
+  max-width: 500px;
+  min-width: 200px;
+  width: 20%;
+  border-right: 3px solid black;
+  padding: 0px 1em;
+}
+
+
+h2 {
+  text-decoration: underline;
+  margin: 1em 0px 0px 0px;
+}
 </style>
