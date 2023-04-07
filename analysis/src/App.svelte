a42d7fc46fd9a0f0976ab51a6f2607ce9c4b3d89
Rithish <rithish.k@akshayahgs.org>
2023-04-08 00:02:15 +0530

    firebase integration

--- analysis/src/App.svelte
+++ analysis/src/App.svelte
@@ -1,5 +1,36 @@
 <script lang="ts">
-import {Link, Router} from "svelte-navigator";
+  import {Link, Router} from "svelte-navigator";
+//   import { initializeApp } from "firebase/app";
+//   import { collection, doc, getFirestore, setDoc } from "firebase/firestore";
+
+function converter(jd) {
+  return {
+    "A*": jd["A*"],
+    "A or above": jd["A or above"],
+    "B or above": jd["B or above"],
+    "C or above": jd["C or above"],
+    "D or above": jd["D or above"],
+    "E or above": jd["E or above"],
+    "AS A-E grades": jd["AS A-E grades"],
+    'ungraded': jd["ungraded"]
+  }
+}
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// const coll = collection(db, "results-statistics");
+// ["s20", "w20", "m21", "s21", "w21", "m22", "s22", "w22"].forEach(season => {
+//   const s = doc(coll, season);
+//   const thresholds = collection(s, "thresholds");
+//   for(let j of json[season]) {
+//     setDoc(doc(thresholds, j.Subject), converter(j))
+//   }
+// })
+// const thresholds = collection(season, "thresholds");
+// for(let j of json) {
+//   setDoc(doc(thresholds, j.Subject), converter(j))
+// }
+
 const tTestLinks: string[] = [
   "Sample t test",
   "Difference in means",
